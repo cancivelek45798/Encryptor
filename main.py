721fd1827628b8bb4173bccf8fcb9f88dd0a0d86
Can Civelek <can.civelek.45798@gmail.com>
2023-06-26 03:04:20 +0300

    encoder-decoder func. added, ui updated, button func. added

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
 import tkinter
+import base64
 
+#functions
+def combine_and_encode(text, password):
+    combined_text = text + password
+    encoded_data = base64.b64encode(combined_text.encode('utf-8'))
+    return encoded_data.decode('utf-8')
+
+def decode_and_extract(encoded_data, password):
+    decoded_data = base64.b64decode(encoded_data.encode('utf-8'))
+    text = decoded_data[:-len(password)]
+    return text.decode('utf-8')
+
+
+def button_save_and_encrypt():
+    title = entry_title.get()
+    text = text_note.get("1.0", tkinter.END) #Bir satırlık boşluk bırakıyor.
+    masterkey = entry_masterkey.get()
+
+    encoded_text = combine_and_encode(text, masterkey)
+
+    save_data(title, encoded_text)
+
+def button_decrypt():
+    encoded_text = text_note.get("1.0", tkinter.END)
+    masterkey = entry_masterkey.get()
+
+    decoded_text = decode_and_extract(encoded_text, masterkey)
+    change_text_content(decoded_text)
+
+def save_data(title, text):
+    with open('data.txt', 'a') as f:
+        data = f"{title}:\n{text}\n\n" #data en son buraya gelecek.
+        f.write(data)
+
+def change_text_content(decoded_text):
+    text_note.delete("1.0", tkinter.END)
+    text_note.insert("1.0", decoded_text)
+
+#window
 window = tkinter.Tk()
 window.minsize(width=400, height=500)
 window.config(pady=50)
 window.title("ENCRYPTOR")
 
 #===== ui =====
+#image
+image = tkinter.PhotoImage(file="topsecret.png")
+label_image = tkinter.Label(image=image)
+label_image.pack()
 #title
 label_title = tkinter.Label(text="Enter your title")
 label_title.pack()
@@ -29,10 +72,10 @@ entry_masterkey = tkinter.Entry(width=30)
 entry_masterkey.pack()
 
 #buttons
-label_se_button = tkinter.Button(text="Save & Encrypt")
+label_se_button = tkinter.Button(text="Save & Encrypt", command=button_save_and_encrypt)
 label_se_button.pack()
 
-label_d_button = tkinter.Button(text="Decrypt")
+label_d_button = tkinter.Button(text="Decrypt", command=button_decrypt)
 label_d_button.pack()
 
 
